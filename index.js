4404ff69c9ce8daf9c0af17a4291f301a99265d7
Nathan Venn <natevenn@gmail.com>
2016-08-14 16:47:47 -0600

    add function to alphabetize by first name

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ hello('es6');
 
 $(document).ready(function() {
   getContacts(contacts)
+
+  document.addEventListener('click', function(event) {
+    buttonId = event.target.getAttribute('id')
+    sortedContacts = sortByNames(buttonId)
+    AddsortedContactsToDom(sortedContacts)
+  });
 });
 
 function getContacts(contacts) {
@@ -21,6 +27,33 @@ function getContacts(contacts) {
     var contact = '<div class="contact"><p class="name">' + contacts[i] + '</p></div>'
     $(contactList).append(contact)
   }
+}
+
+function AddsortedContactsToDom(sortedContacts) {
+  var contactList = document.getElementsByClassName('contact')
+  for(var i=0; i<contactList.length; i++){
+    oldName = contactList[i].getElementsByClassName('name')
+    newName = sortedContacts[i]
+    $(oldName).text(newName)
+  }
+}
+
+function sortByNames(name) {
+  var element = 0
+  if (name === 'last-name') {
+    element = -1
+  }
+  return contacts.sort(function(a, b) {
+    nameA = a.split(' ')[element]
+    nameB = b.split(' ')[element]
+    if(nameA < nameB){
+      return -1
+    }
+    if(nameA > nameB){
+      return 1
+    }
+    return 0
+  })
 }
 
 var contacts = [
